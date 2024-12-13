877d7e9de4ed02f7d2b6068a1928e2d3457dd08a
DIEGO DOMINGOS SILVA <122846344+digo7586@users.noreply.github.com>
2024-12-13 16:51:07 -0300

    first commit

---
+++ assets/js/dados.js
@@ -0,0 +1,446 @@
+var MENU = {
+    "burgers": [
+        {
+            "id": "01-AmericanaM",
+            "img": "assets/img/cardapio/burguers/gaguinho.jpg",
+            "name": "Lanche Costela",
+            "title": "Pão baguete, costela, queijo, cebola, bacon, patê de alho, queijo prato, alface tomate, ketchup, maionese.",
+            "dsc": " M 8 pdç",
+            "price": 30.00
+        },
+        {
+            "id": "02-AtumM",
+            "img": "assets/img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
+            "name": "Lanche Linguiça",
+            "title": "Pão baguete, linguiça, queijo, cebola, bacon, patê de alho queijo prato, rúcula, tomate ketchup, maionese.",
+            "dsc": " M 8 pdç",
+            "price": 25.00
+        },
+        {
+            "id": "03-BaconM",
+           "img": "assets/img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
+            "name": "Lanche Frango",
+            "title": "Pão baguete, frango, queijo, cebola, bacon, patê de alho queijo prato, alface, tomate ketchup, maionese.",
+            "dsc": " M 8 pdç",
+            "price": 22.00
+        },
+        {
+            "id": "04-BaianinhaM",
+            "img": "assets/img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
+            "name": "Lanche Contra filé",
+            "title": "Pão baguete, contra filé, queijo, cebola, bacon, patê de alho queijo prato, alface, tomate ketchup, maionese.",
+            "dsc": " M 8 pdç",
+            "price": 30.00
+        },
+        {
+            "id": "05-BauruM",
+            "img": "assets/img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
+            "name": "Hambúrguer gourmet",
+            "title": "Pão de hambúrguer, 150g hambúrguer artesanal, bacon, queijo prato patê de alho, alface, tomate ketchup, maionese.",
+            "dsc": " M 8 pdç",
+            "price": 20.00
+        },
+        {
+            "id": "06-BrasileiraM",
+            "img": "assets/img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
+            "name": "Hambúrguer simples",
+            "title": "Pão de hambúrguer, hambúrguer tradicional, queijo mussarela, alface, tomate ketchup, maionese.",
+            "dsc": " M 8 pdç",
+            "price": 15.00
+        },
+        {
+            "id": "07-BrócolisM",
+            "img": "assets/img/cardapio/burguers/handf-double-stack-burger-kit-for-4.4ee9f54b1d6087e9996335f07c13e5cd.jpg",
+            "name": "Cachorro quente",
+            "title": "Alface, tomate, milho, batata palha, salsicha, ketchup, maionese.",
+            "dsc": " M 8 pdç",
+            "price": 16.00
+        },
+        {
+            "id": "08-CalabresalM",
+            "img": "assets/img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
+            "name": "Lanche Calabresa",
+            "title": "Alface, tomate, cebola, calabresa, queijo, ketchup, maionese, chedder.",
+            "dsc": " M 8 pdç",
+            "price": 23.00
+        },
+        {
+            "id": "09-CalabresaEspecialM",
+            "img": "assets/img/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg",
+            "name": "Lanche Pernil",
+            "title": "Rúcula, cebola, pernil, vinagrete, barbecue.",
+            "dsc": " M 8 pdç",
+            "price": 25.00
+        },
+        {
+            "id": "10-ContraFileM",
+            "img": "assets/img/cardapio/burguers/gaguinho.jpg",
+            "name": "Lanche Coração",
+            "title": "Alface, tomate, cebola, coração, ketchup, maionese.",
+            "dsc": "M 8 pdç",
+            "price": 30.00
+        },
+        {
+            "id": "11-CrocanteM",
+            "img": "assets/img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
+            "name": "Hambúrguer Duplo",
+            "title": "hambúrguer, alface, tomate, bacon, queijo.",
+            "dsc": "M 8 pdç",
+            "price": 18.00
+        },
+        {
+            "id": "12-FrangoCatupiryP",
+            "img": "assets/img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
+            "name": "cachorrão",
+            "title": "Alface, tomate, bacon, milho, batata palha, 2 salsicha, ketchup, maionese.",
+            "dsc": "P 4 pdç",
+            "price": 22.00
+        }
+    ],
+    /* "Pastel": [
+        {
+            "id": "38-banana-nevada",
+            "img": "assets/img/cardapio/pizzas/bananaNevada.png",
+            "name": "Banana Nevada",
+            "title": "Banana, chocolate branco, canela e açúcar.",
+             "dsc": "P 4 pdç",
+            "price":  37.00,
+               
+            
+        },
+        {
+            "id": "39-brigadeiro",
+            "img": "assets/img/cardapio/pizzas/brigadeiro.png",
+            "name": "Brigadeiro",
+            "title": "Chocolate com granulado.",
+            "dsc": "P 4 pdç",
+            "price": 
+                 37.00,
+                
+        },
+        {
+            "id": "40-chocobana",
+            "img": "assets/img/cardapio/pizzas/chocobana.png",
+            "name": "Chocobana",
+            "title": "Leite condensado, mussarela, banana e chocolate.",
+             "dsc": "P 4 pdç",
+            "price": 
+                 37.00,
+                
+        },
+        {
+            "id": "41-confete",
+            "img": "assets/img/cardapio/pizzas/confete.png",
+            "name": "Confete",
+            "title": "Chocolate com confete.",
+             "dsc": "P 4 pdç",
+            "price": 39.00,
+                
+        },
+        {
+            "id": "42-dois-amores",
+            "img": "assets/img/cardapio/pizzas/doisAmores.png",
+            "name": "Dois Amores",
+            "title": "Chocolate branco e chocolate preto.",
+             "dsc": "P 4 pdç",
+            "price":37.00,
+                
+            
+        },
+        {
+            "id": "43-napolitana",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Napolitana",
+            "title": "Ninho, creme de morango e chocolate ao leite.",
+             "dsc": "P 4 pdç",
+            "price": 39.00,
+                
+        },
+        {
+            "id": "44-leite-ninho",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Leite Ninho",
+            "title": "Chocolate com creme de leite ninho.",
+             "dsc": "P 4 pdç",
+            "price": 39.00,
+                
+        },
+        {
+            "id": "45-ouro-branco",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Ouro Branco",
+            "title": "Chocolate com bombom Ouro Branco.",
+             "dsc": "P 4 pdç",
+            "price":  39.00,
+                
+            
+        },
+        {
+            "id": "46-prestigio",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Prestígio",
+            "title": "Chocolate com coco ralado.",
+             "dsc": "P 4 pdç",
+            "price":  37.00,
+                
+        },
+        {
+            "id": "47-morango",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Morango",
+            "title": "Chocolate e morango fruta.",
+             "dsc": "P 4 pdç",
+            "price":  39.00,
+                
+            
+        },
+        {
+            "id": "48-surpresa-uva",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Surpresa de Uva Novidade",
+            "title": "Delicioso chocolate ao leite, uvas verdes sem caroço e um maravilhoso creme de ninho.",
+             "dsc": "P 4 pdç",
+            "price": 39.00,
+                
+        },
+        {
+            "id": "49-abacaxi-nevado",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Abacaxi Nevado Novidade",
+            "title": "Delicioso abacaxi com chocolate branco e uma pitada de canela.",
+             "dsc": "P 4 pdç",
+            "price": 39.00,
+                
+        }
+    ],
+    "pizzasSweetM": [
+        {
+            "id": "38-banana-nevadaM",
+            "img": "assets/img/cardapio/pizzas/bananaNevada.png",
+            "name": "Banana Nevada",
+            "title": "Banana, chocolate branco, canela e açúcar.",
+            "dsc": "M 8 pdç",
+            "price":
+                 52.00,
+               
+        },
+        {
+            "id": "39-brigadeiroM",
+            "img": "assets/img/cardapio/pizzas/brigadeiro.png",
+            "name": "Brigadeiro",
+            "title": "Chocolate com granulado.",
+             "dsc": "M 8 pdç",
+            "price":  52.00,
+               
+        },
+        {
+            "id": "40-chocobanaM",
+            "img": "assets/img/cardapio/pizzas/chocobana.png",
+            "name": "Chocobana",
+            "title": "Leite condensado, mussarela, banana e chocolate.",
+             "dsc": "M 8 pdç",
+            "price":  52.00,
+               
+        },
+        {
+            "id": "41-confeteM",
+            "img": "assets/img/cardapio/pizzas/confete.png",
+            "name": "Confete",
+            "title": "Chocolate com confete.",
+             "dsc": "M 8 pdç",
+            "price": 54.00,
+                
+        },
+        {
+            "id": "42-dois-amoresM",
+            "img": "assets/img/cardapio/pizzas/doisAmores.png",
+            "name": "Dois Amores",
+            "title": "Chocolate branco e chocolate preto.",
+             "dsc": "M 8 pdç",
+            "price":52.00,
+                
+        },
+        {
+            "id": "43-napolitanaM",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Napolitana",
+            "title": "Ninho, creme de morango e chocolate ao leite.",
+             "dsc": "M 8 pdç",
+            "price": 54.00,
+                
+        },
+        {
+            "id": "44-leite-ninhoM",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Leite Ninho",
+            "title": "Chocolate com creme de leite ninho.",
+             "dsc": "M 8 pdç",
+            "price": 54.00,
+                
+        },
+        {
+            "id": "45-ouro-brancoM",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Ouro Branco",
+            "title": "Chocolate com bombom Ouro Branco.",
+             "dsc": "M 8 pdç",
+            "price": 54.00,
+               
+        },
+        {
+            "id": "46-prestigioM",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Prestígio",
+            "title": "Chocolate com coco ralado.",
+             "dsc": "M 8 pdç",
+            "price": 52.00,
+                
+        },
+        {
+            "id": "47-morangoM",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Morango",
+            "title": "Chocolate e morango fruta.",
+             "dsc": "M 8 pdç",
+            "price":54.00,
+               
+        },
+        {
+            "id": "48-surpresa-uvaM",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Surpresa de Uva Novidade",
+            "title": "Delicioso chocolate ao leite, uvas verdes sem caroço e um maravilhoso creme de ninho.",
+             "dsc": "M 8 pdç",
+            "price": 54.00,
+                
+        },
+        {
+            "id": "49-abacaxi-nevadoM",
+            "img": "assets/img/cardapio/pizzas/pizzaDoce.webp",
+            "name": "Abacaxi Nevado Novidade",
+            "title": "Delicioso abacaxi com chocolate branco e uma pitada de canela.",
+             "dsc": "M 8 pdç",
+            "price": 54.00,
+               
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "sorvete-taca",
+            "img": "assets/img/cardapio/sobremesas/sorve.png",
+            "name": "Sorvete Taça",
+            "title": "Consulte sabores.",
+            "dsc": "",
+            "price": 15.00
+        },
+        {
+            "id": "milk-shake",
+            "img": "assets/img/cardapio/sobremesas/milk.png",
+            "name": "Milk Shake",
+            "title": "Sabores: Ovomaltine e Morango.",
+            "dsc": "",
+            "price": 20.00
+        },
+        {
+            "id": "acai-taca",
+            "img": "assets/img/cardapio/sobremesas/acai.png",
+            "name": "Açaí Taça",
+            "title": "A taça vai com dois acompanhamentos grátis. Acompanhamentos adicionais R$ 3,00 cada.",
+            "dsc": "",
+            "price": 15.00
+        }
+    ], */
+    "bebidas": [
+        {
+            "id": "refrigerante-lata",
+            "img": "assets/img/cardapio/refri.png",
+            "name": "Refrigerante Lata 350ml",
+            "title": "Refrigerante em Lata. Consulte sabores.",
+            "dsc": "",
+            "price": 6.00
+        },
+        {
+            "id": "coca-cola-2l",
+            "img": "assets/img/cardapio/sucoNat.jpg",
+            "name": "Suco natural 300ml",
+            "title": "Sucos naturais. consulte sabores",
+            "dsc": "",
+            "price": 6.00
+        },
+        {
+            "id": "agua-com-gas",
+            "img": "assets/img/cardapio/cerva3.jpg",
+            "name": "Cerveja lata 350ml",
+            "title": "Água Mineral com Gás",
+            "dsc": "",
+            "price": 7.00
+        },
+        {
+            "id": "agua-sem-gas",
+            "img": "assets/img/cardapio/agua.png",
+            "name": "Água sem gás",
+            "title": "Água Mineral Sem Gás",
+            "dsc": "",
+            "price": 4.00
+        },
+        {
+            "id": "xereta-2l",
+            "img": "assets/img/cardapio/choop.jpg",
+           "name": "Chopp",
+            "title": "",
+            "dsc": "",
+            "price": 10.00
+        }, 
+        
+       
+        /*
+        {
+            "id": "suco-del-valle",
+            "img": "assets/img/cardapio/suco.png",
+            "name": "Suco Del Valle",
+            "title": "Suco Del Valle com diversos sabores.",
+            "dsc": "",
+            "price": 6.00
+        }, 
+        {
+            "id": "cervejas-long-neck",
+            "img": "assets/img/cardapio/longNeck.png",
+            "name": "Cervejas Long Neck",
+            "title": "Budweiser, Heineken e Corona",
+            "dsc": "",
+            "price": 9.00
+        },
+        {
+            "id": "cerveja-skol-600ml",
+            "img": "assets/img/cardapio/skol.png",
+            "name": "Skol/Antarctica 600ml",
+            "title": "Cerveja Skol ou Antarctica 600ml",
+            "dsc": "",
+            "price": 12.00
+        },
+        {
+            "id": "cerveja-original-600ml",
+            "img": "assets/img/cardapio/original.png",
+            "name": "Original/Heineken 600ml",
+            "title": "Cerveja Original ou Heineken 600ml",
+            "dsc": "",
+            "price": 15.00
+        },
+        {
+            "id": "taca-de-vinho",
+            "img": "assets/img/cardapio/wine.png",
+            "name": "Taça de Vinho",
+            "title": "Taça de Vinho",
+            "dsc": "",
+            "price": 10.00
+        },
+        {
+            "id": "caipirinha",
+            "img": "assets/img/cardapio/caipirinha.png",
+            "name": "Caipirinha",
+            "title": "Caipirinha",
+            "dsc": "",
+            "price": 12.00
+        } */
+    ]
+}
